d2e0a3731dbb075416ec1fc84a4e6c7ab684e78c
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2024-09-07 23:54:33 +0300

    Implemented WeatherMiniApp interface for interacting with external entities

--- Sources/WeatherMiniApp/WeatherMiniApp.swift
+++ Sources/WeatherMiniApp/WeatherMiniApp.swift
@@ -1,2 +1,29 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import UIKit
+import MiniAppCore
+
+public final class WeatherMiniApp: MiniAppProtocol {
+    
+    // MARK: - Public Properties
+    
+    public var title: String { Const.miniAppTitle }
+    
+    public var icon: UIImage { UIImage(systemName: Const.miniAppIcon) ?? UIImage() }
+    
+    public var view: UIView { WeatherView.create() }
+    
+    // MARK: - Public Initializers
+    
+    public init() {}
+    
+    // MARK: - Public Methods
+    
+    public func configure(with configuration: MiniAppConfiguration) {
+        if let weatherView = view as? WeatherView {
+            weatherView.configure(
+                with: configuration.backgroundColor,
+                textColor: configuration.textColor,
+                textFont: configuration.font
+            )
+        }
+    }
+}
