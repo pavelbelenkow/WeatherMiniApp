30202d8965eb276b0c3f4bab4b01a0afb3bfc025
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2024-09-07 23:35:47 +0300

    Added MiniAppCore dependency to package

--- Package.swift
+++ Package.swift
@@ -5,17 +5,23 @@ import PackageDescription
 
 let package = Package(
     name: "WeatherMiniApp",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "WeatherMiniApp",
             targets: ["WeatherMiniApp"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/pavelbelenkow/MiniAppCore", from: "1.0.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "WeatherMiniApp"),
+            name: "WeatherMiniApp",
+            dependencies: ["MiniAppCore"]
+        ),
         .testTarget(
             name: "WeatherMiniAppTests",
             dependencies: ["WeatherMiniApp"]),
